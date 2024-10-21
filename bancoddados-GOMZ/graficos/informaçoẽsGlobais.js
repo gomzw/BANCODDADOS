9fbee2772116209a78dd0562f3d2c466be388002
gomz01 <111518206+gomzw@users.noreply.github.com>
2024-10-21 11:05:05 -0300

    Add files via upload

--- bancoddados-GOMZ/graficos/informaçoẽsGlobais.js
+++ bancoddados-GOMZ/graficos/informaçoẽsGlobais.js
@@ -8,3 +8,23 @@ async function visualizarInformacoesGlobais(){
     const res = await fetch(url);
     const dados = await res.json;
 }
+
+const layout = {
+  
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title:{
+        text: 'Redes socias com mais usuarios'
+        x: 0,
+        font: {
+            color:getCSS(--primary-color),
+            size: 30,
+            font: getCSS('--font')
+        }
+    }
+    xaxis: {
+        tickfont:tickConfig,
+    }
+
+
+}
