0ef993058d068dcab63367760e5923d704d7a5dc
gomz01 <111518206+gomzw@users.noreply.github.com>
2024-10-23 09:05:52 -0300

    Add files via upload

--- bancoddados-GOMZ/graficos/informaçoẽsGlobais.js
+++ bancoddados-GOMZ/graficos/informaçoẽsGlobais.js
@@ -1,43 +1,18 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+const url= 'https://raw.githubusercontent.com/guilhermemeonrails/api/main/dados-globais.json';
 
 //url='20.201.28.151/main/dados/grafico-principal.json';
 //modelo de endereço servidor de dados para o javaScript.
 
-async function visualizarInformacoesGlobais(){
 
+async function visualizarinformacoesGlobais() {
     const res = await fetch(url);
-    const dados = await res.json;
+    const dados = await res.json
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_de_pessoas_mundo / 1e9)
+    console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_de_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão em alguma rede social e passam em média <span>${dados.tempo_medio}</span>horas conectadas.`
 }
 
-const layout = {
-  
-    plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color'),
-    title:{
-        text: 'Redes socias com mais usuarios',
-        x: 0,
-        font: {
-            color:getCSS(--primary-color),
-            size: 30,
-            font: getCSS('--font')
-        }
-    },
-    xaxis: {
-        tickfont: tickConfig,
-        title: {
-            text: 'Nome das redes',
-            font: {
-                color: getCSS('--secondary-color')
-            }
-        }
-    },
-    yaxis: {
-        tickfont: tickConfig,
-        title: {
-          text: 'Bilhões de usuários ativos',
-          font: {
-              color: getCSS('--secondary-color')
-          }
-        }
-      }
-}
+visualizarInformacoesGlobais();
